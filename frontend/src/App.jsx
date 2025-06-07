7d55dbdb3aa4c19f67b02458d22c12171852fa98
thao-nguyenthithu <thaonguyenthithu.ws@gmail.com>
2025-06-07 17:41:21 +0700

    init

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-blue-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind hoạt động rồi! 
+      </h1>
+    </div>
+  );
+}
